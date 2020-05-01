613fed3f6957f499ba93ae6757d67f528ee112a9
Vladyslav <vladyslav.kondratiev@eleks.com>
2020-05-01 17:37:01 +0300

    linked list removeTail

--- 1_linked_list/index.js
+++ 1_linked_list/index.js
@@ -40,3 +40,15 @@ LinkedList.prototype.removeHead = function () {
   }
   return val;
 };
+
+LinkedList.prototype.removeTail = function () {
+  if (!this.tail) return null;
+  let val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+  return val;
+};
